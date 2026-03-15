1d8d7503a1340de6482d1bb769f313fcd7e96791
sanfazy <santifauzi7@gmail.com>
2026-03-15 14:59:21 +0700

    v3

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbzRwpT0EN4vAezG8qVhKXSaLsjskC_P6wy85xf9gYi89neuv6z7PNrUkyAcIQOCPkTyLA/exec";
+const API = "https://script.google.com/macros/s/AKfycbzRwpT0EN4vAezG8qVhKXSaLsjskC_P6wy85xf9gYi89neuv6z7PNrUkyAcIQOCPkTyLA/exec";
 
 function trackerApp(){
 
 return{
 
-menu:'dashboard',
+menu:"dashboard",
 
 openMenu:false,
 openSholat:false,
 openPuasa:false,
 openModalQodo:false,
 
-sholatList:[],
-puasaList:[],
+activeTab:"text-green-600 border-b-2 border-green-600",
+
+sholat:[],
+puasa:[],
+
+tahunList:[],
+tahunFilter:"",
 
 hutangSholat:0,
 hutangPuasa:0,
-
-tahunFilter:"",
-tahunList:[],
 
 formSholat:{
 tanggal:"",
@@ -36,62 +38,86 @@ type:"",
 tanggal_qodo:""
 },
 
-get sholatBelum(){
+init(){
 
-return this.sholatList.filter(i => 
-i[4]=='' && i[3]==this.tahunFilter
-)
+this.loadData()
 
-},
-
-get puasaBelum(){
-
-return this.puasaList.filter(i => 
-i[4]=='' && i[3]==this.tahunFilter
-)
+this.$watch('tahunFilter',()=>{
+this.hitungStat()
+})
 
 },
 
-get sholatSudah(){
+/* API */
 
-return this.sholatList.filter(i => 
-i[4] != '' && i[3]==this.tahunFilter
-)
+async apiGet(type){
+
+const res = await fetch(`${API}?type=${type}`)
+return res.json()
+
+},
+
+async apiPost(data){
+
+await fetch(API,{
+method:"POST",
+body:JSON.stringify(data)
+})
 
 },
 
-get puasaSudah(){
-
-return this.puasaList.filter(i => 
-i[4] != '' && i[3]==this.tahunFilter
-)
-
-},
+/* LOAD DATA */
 
 async loadData(){
 
-let s = await fetch(API_URL+"?type=sholat")
-let p = await fetch(API_URL+"?type=puasa")
+const [sholat,puasa] = await Promise.all([
+this.apiGet("sholat"),
+this.apiGet("puasa")
+])
 
-this.sholatList = await s.json()
-this.puasaList = await p.json()
+this.sholat = sholat
+this.puasa = puasa
 
 this.generateTahun()
-
 this.hitungStat()
 
 },
 
+/* COMPUTED */
+
+get sholatBelum(){
+return this.sholat.filter(i =>
+!i.tanggal_qodo && i.tahun==this.tahunFilter
+)
+},
+
+get puasaBelum(){
+return this.puasa.filter(i =>
+!i.tanggal_qodo && i.tahun==this.tahunFilter
+)
+},
+
+get sholatSudah(){
+return this.sholat.filter(i =>
+i.tanggal_qodo && i.tahun==this.tahunFilter
+)
+},
+
+get puasaSudah(){
+return this.puasa.filter(i =>
+i.tanggal_qodo && i.tahun==this.tahunFilter
+)
+},
+
+/* ACTION */
+
 async saveSholat(){
 
-await fetch(API_URL,{
-method:"POST",
-body:JSON.stringify({
+await this.apiPost({
 action:"add",
 type:"sholat",
 tanggal:this.formSholat.tanggal,
 nama:this.formSholat.nama
-})
 })
 
 this.openSholat=false
@@ -101,14 +127,11 @@ this.loadData()
 
 async savePuasa(){
 
-await fetch(API_URL,{
-method:"POST",
-body:JSON.stringify({
+await this.apiPost({
 action:"add",
 type:"puasa",
 tanggal:this.formPuasa.tanggal,
 nama:this.formPuasa.nama
-})
 })
 
 this.openPuasa=false
@@ -118,7 +141,7 @@ this.loadData()
 
 openQodo(item,type){
 
-this.formQodo.id=item[0]
+this.formQodo.id=item.id
 this.formQodo.type=type
 
 this.openModalQodo=true
@@ -127,14 +150,11 @@ this.openModalQodo=true
 
 async saveQodo(){
 
-await fetch(API_URL,{
-method:"POST",
-body:JSON.stringify({
+await this.apiPost({
 action:"qodo",
 type:this.formQodo.type,
 id:this.formQodo.id,
 tanggal_qodo:this.formQodo.tanggal_qodo
-})
 })
 
 this.openModalQodo=false
@@ -142,40 +162,31 @@ this.loadData()
 
 },
 
+/* UTIL */
+
 generateTahun(){
 
-let tahun = []
+let tahun=[
+...this.sholat.map(i=>i.tahun),
+...this.puasa.map(i=>i.tahun)
+]
 
-this.sholatList.forEach(i=>{
-if(i[3]) tahun.push(i[3])
-})
+tahun=[...new Set(tahun)].sort((a,b)=>b-a)
 
-this.puasaList.forEach(i=>{
-if(i[3]) tahun.push(i[3])
-})
-
-tahun = [...new Set(tahun)]
-
-tahun.sort((a,b)=>b-a)
-
-this.tahunList = tahun
+this.tahunList=tahun
 
 if(!this.tahunFilter){
-this.tahunFilter = tahun[0]
+this.tahunFilter=tahun[0]
 }
 
 },
 
 hitungStat(){
 
-this.hutangSholat = this.sholatBelum.length
-this.hutangPuasa = this.puasaBelum.length
+this.hutangSholat=this.sholatBelum.length
+this.hutangPuasa=this.puasaBelum.length
 
-},
-
-$watch('tahunFilter', () => {
-this.hitungStat()
-})
+}
 
 }
 
